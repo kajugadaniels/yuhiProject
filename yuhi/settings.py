60091e40be1650c7a658cd056d26548d9f41f887
kajugadaniels <kajugadaniels@gmail.com>
2024-03-16 13:32:38 +0200

    Create vendor app

--- yuhi/settings.py
+++ yuhi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     
     # Custom Apps
     'account.apps.AccountConfig',
+    'store',
 ]
 
 MIDDLEWARE = [
--- yuhi/urls.py
+++ yuhi/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vendor/', include('store.urls')),
     path('account/', include('account.urls')),
 ]
 
