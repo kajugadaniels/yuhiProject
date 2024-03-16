6b8d7f76f6dd4e7e2a45214b857cbb85d1373cd0
Munyeshya <obedbenny54@gmail.com>
2024-03-16 15:57:40 +0200

    Create vendors model

--- vendor/models.py
+++ vendor/models.py
@@ -1,10 +1,10 @@
-import shortuuid
 from django.db import models
 from account.models import User
+from shortuuidfield import ShortUUIDField
 
 class Vendor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    vid = models.CharField(max_length=30, unique=True, editable=False, default=f'vendor_{shortuuid.ShortUUID().random(length=10)}')
+    vid = models.CharField(max_length=30, unique=True, editable=False, default=f'vendor_{shortuuidfield.uuid()[:10]}')
     store_name = models.CharField(max_length=255)
     manager_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
