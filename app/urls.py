1e4cac942a4aeb1ec01c491190bd9fddd0550e4b
kajugadaniels <kajugadaniels@gmail.com>
2024-03-16 13:53:47 +0200

    Create urls.py file to app app and add app templates to views

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from app import views
 app_name = 'app'
 
 urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('about/', views.about, name = 'about'),
+    path('contact/', views.contact, name = 'contact'),
+    path('search/', views.search, name = 'search'),
 ]
